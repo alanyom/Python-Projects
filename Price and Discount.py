8c5266bc693c5101509087cd22aa9ba7ad64fbc9
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 11:01:27 -0800

    Create Price and Discount.py

---
+++ Price and Discount.py
@@ -0,0 +1,7 @@
+# Price and Discount / Format method
+
+origPrice = float(input('Enter the original price: $'))
+discount = float(input('Enter discount percentage: '))
+newPrice = (1 - discount/100)*origPrice
+calculation = '${} discounted by {}% is ${}.'.format(origPrice, discount, newPrice)
+print(calculation)
