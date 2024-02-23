64f6cc0b370b8292384f592283197d170163cf3e
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 11:04:03 -0800

    Create Appending Each Char to List.py

---
+++ Appending Each Char to List.py
@@ -0,0 +1,7 @@
+#For each character in the string saved in ael, append that character to a list that should be saved in a variable app.
+
+ael = "python!"
+app = []
+for x in ael:
+    app.append(x)
+print(app)
