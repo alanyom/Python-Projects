e2131afb65cef49543e632c0eff2a08480e1c87a
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 09:20:59 -0800

    Create Finding Min and Max Word Occurences.py

---
+++ Finding Min and Max Word Occurences.py
@@ -0,0 +1,18 @@
+#accumulating max and min value
+
+#Create a dictionary called d that keeps track of all the characters in the string placement and notes how many times each character was seen. Then, find the key with the lowest value in this dictionary and assign that key to min_value.
+
+placement = "Beaches are cool places to visit in spring however the Mackinaw Bridge is near. Most people visit Mackinaw later since the island is a cool place to explore."
+
+d = {}
+
+for c in placement:
+    if c not in d:
+        d[c] = 0
+    d[c] += 1
+    
+keys = list(d.keys())
+min_value = keys[0]
+for x in keys:
+    if d[x] < d[min_value]:
+        min_value = x
