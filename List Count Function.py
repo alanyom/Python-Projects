bc7a47200af359fa3dce48f5d23d13a9aeaba507
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 09:38:18 -0800

    Create List Count Function.py

---
+++ List Count Function.py
@@ -0,0 +1,6 @@
+#Write a function called count that takes a list of numbers as input and returns a count of the number of elements in the list.
+
+def count(b):
+    x = len(b)
+    return x
+print(count([1,2,3]))
