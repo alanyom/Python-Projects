041c0f7b54001c94906737b6d64bb794d97a4efe
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 08:52:36 -0800

    Create Aliasing and Copying.py

---
+++ Aliasing and Copying.py
@@ -0,0 +1,9 @@
+#Aliasing and Copying
+
+opposites = {'up': 'down', 'right': 'wrong', 'true': 'false'}
+alias = opposites
+
+print(alias is opposites)
+
+alias['right'] = 'left'
+print(opposites['right'])
