c58e6fab382ef3658508a6c8889e85035fda16f1
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 08:41:16 -0800

    Create Opening CSV File.py

---
+++ Opening CSV File.py
@@ -0,0 +1,3 @@
+#You are in a directory called Project. In it, you have a file with your Python code. You would like to read in data from a file called “YearlyProjections.csv” which is in a folder called CompanyData, which is inside of Project. What is the best way to open the file in your Python program?
+
+ open("CompanyData/YearlyProjections.csv", "r")
