f7ad878e0ef40cce3bb2cfbe38cd29fade0c266a
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 09:50:54 -0800

    Create Third of Every Tuple.py

---
+++ Third of Every Tuple.py
@@ -0,0 +1,7 @@
+#Provided is a list of tuples. Create another list called t_check that contains the third element of every tuple.
+
+t_check = []
+lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizard', 'Venasaur', 'Squirtle'), ('Oddish', 'Poliwag', 'Diglett', 'Bellsprout'), ('Ponyta', "Farfetch'd", "Tauros", 'Dragonite'), ('Hoothoot', 'Chikorita', 'Lanturn', 'Flaaffy', 'Unown', 'Teddiursa', 'Phanpy'), ('Loudred', 'Volbeat', 'Wailord', 'Seviper', 'Sealeo')]
+for pokemon in lst_tups:
+    t_check.append(pokemon[2])
+print(t_check)
