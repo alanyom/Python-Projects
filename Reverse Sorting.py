54e6255ec05056151ead1278f96b0490ad5468d4
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 10:42:58 -0800

    Create Reverse Sorting.py

---
+++ Reverse Sorting.py
@@ -0,0 +1,8 @@
+#reverse sorting
+L2 = ["Cherry", "Apple", "Blueberry"]
+print(sorted(L2, reverse=True))
+
+#Sort the list, lst from largest to smallest. Save this new list to the variable lst_sorted.
+
+lst = [3, 5, 1, 6, 7, 2, 9, -2, 5]
+lst_sorted = sorted(lst, reverse=True)
