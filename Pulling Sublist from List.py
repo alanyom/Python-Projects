84c0390c483c54239df9f7ad84d9e8d08e7a80f4
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 11:14:33 -0800

    Create Pulling Sublist from List.py

---
+++ Pulling Sublist from List.py
@@ -0,0 +1,4 @@
+#Create a new list using the 9th through 12th elements (four items in all) of new_lst and assign it to the variable sub_lst.
+
+new_lst = ["computer", "luxurious", "basket", "crime", 0, 2.49, "institution", "slice", "sun", ["water", "air", "fire", "earth"], "games", 2.7, "code", "java", ["birthday", "celebration", 1817, "party", "cake", 5], "rain", "thunderstorm", "top down"]
+sub_lst = (new_lst[8:12])
