296729c89e6f31081c8b60727d48159221c62322
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 08:50:47 -0800

    Create Adding to Dictionary.py

---
+++ Adding to Dictionary.py
@@ -0,0 +1,7 @@
+#Adding to a Dictionary / Updating a dictionary
+
+#Update the value for “Phelps” in the dictionary swimmers to include his medals from the Rio Olympics by adding 5 to the current value (Phelps will now have 28 total medals). Do not rewrite the dictionary.
+
+
+swimmers = {'Manuel':4, 'Lochte':12, 'Adrian':7, 'Ledecky':5, 'Dirado':4, 'Phelps':23}
+swimmers['Phelps'] += 5
