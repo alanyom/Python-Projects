be3a9873ffe3f3f8defac770b5c0eab93775e380
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-23 10:46:42 -0800

    Create Sorting Dictionary by Key.py

---
+++ Sorting Dictionary by Key.py
@@ -0,0 +1,5 @@
+#Sort the following dictionary based on the keys so that they are sorted a to z. Assign the resulting value to the variable sorted_keys.
+
+
+dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+sorted_keys = sorted(dictionary)
