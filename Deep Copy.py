a8c73fde29c5cb25c956cebb2402c95c96d33e8b
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-25 08:22:22 -0800

    Create Deep Copy.py

---
+++ Deep Copy.py
@@ -0,0 +1,14 @@
+#Deep Copy
+
+import copy
+original = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
+shallow_copy_version = original[:]
+deeply_copied_version = copy.deepcopy(original)
+original.append("Hi there")
+original[0].append(["marsupials"])
+print("-------- Original -----------")
+print(original)
+print("-------- deep copy -----------")
+print(deeply_copied_version)
+print("-------- shallow copy -----------")
+print(shallow_copy_version)
