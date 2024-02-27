a370cc6194d65190f77f9f448a860632c1f76ca2
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-26 18:18:06 -0800

    Update Indexing Nested List.py

--- Indexing Nested List.py
+++ Indexing Nested List.py
@@ -1,4 +1,4 @@
-The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
+#The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
 
 nested = [['dog', 'cat', 'horse'], ['frog', 'turtle', 'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]
 output = nested[1][2]
