29debf4ad9cbe96d6d4cfb527826899e299a0afe
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-26 18:37:57 -0800

    Create Possibility Guesses with Zip.py

---
+++ Possibility Guesses with Zip.py
@@ -0,0 +1,14 @@
+#Possibility guesses with zip
+
+def possible(word, blanked, guesses_made):
+    if len(word) != len(blanked):
+        return False
+    for (bc, wc) in zip(blanked, word):
+        if bc == '_' and wc in guesses_made:
+            return False
+        elif bc != '_' and bc != wc:
+            return False
+    return True
+
+print(possible("wonderwall", "_on__r__ll", "otnqurl"))
+print(possible("wonderwall", "_on__r__ll", "wotnqurl"))
