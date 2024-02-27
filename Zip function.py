d227af69274a5d9e1c69790885913405fcba2e47
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-02-26 18:36:30 -0800

    Create Zip function.py

---
+++ Zip function.py
@@ -0,0 +1,12 @@
+#Zip function (plus list comprehension)
+
+L1 = [3, 4, 5]
+L2 = [1, 2, 3]
+	L3 = [x1 + x2 for (x1, x2) in list(zip(L1, L2))]
+print(L3)
+#Map and Zip function
+
+L1 = [3, 4, 5]
+L2 = [1, 2, 3]
+L3 = map(lambda x: x[0] + x[1], zip(L1, L2))
+print(L3)
